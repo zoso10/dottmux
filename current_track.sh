a555706e834dc755aa2cdfa5b36b682cb53dc7cf
Tyler Ewing <tewing10@gmail.com>
2026-07-20 12:16:50 -0500

    feat: more reliably get track metadata via `playerctl`

--- current_track.sh
+++ current_track.sh
@@ -1,56 +1,16 @@
 #!/usr/bin/env bash
-MAC_NOW_PLAYING=$(osascript <<EOF
-set spotify_state to false
-set itunes_state to false
 
-if is_app_running("Spotify") then
-  tell application "Spotify" to set spotify_state to (player state as text)
-end if
-if is_app_running("iTunes") then
-  tell application "iTunes" to set itunes_state to (player state as text)
-end if
-(* Whatever other music applications you use *)
+# exclude tandem
+tandem_identifier=$(busctl --user list | grep -i mpris | grep tandem | awk '{print $1}' | awk -F. '{print $NF}')
+tandem_player="chromium.${tandem_identifier}"
 
-if spotify_state is equal to "playing" then
-  tell application "Spotify"
-    set track_name to name of current track
-    set artist_name to artist of current track
-    return track_name & " - #[bold]" & artist_name & "#[nobold]"
-  end tell
-else if itunes_state is equal to "playing" then
-  tell application "iTunes"
-    set track_name to name of current track
-    set artist_name to artist of current track
-    return track_name & " - #[bold]" & artist_name & "#[nobold]"
-  end tell
+player_status=$(playerctl --ignore-player=${tandem_player} status 2>/dev/null)
+
+if [[ "$player_status" == "Playing" ]]; then
+  artist=$(playerctl metadata artist 2>/dev/null)
+  song=$(playerctl metadata title 2>/dev/null)
+
+  echo "${song} - #[bold]${artist}#[nobold]"
 else
-  return "Nothing playing :("
-end if
-
-on is_app_running(app_name)
-  tell application "System Events" to (name of processes) contains app_name
-end is_app_running
-EOF)
-
-LINUX_NOW_PLAYING=$(ruby -e '
-require "dbus"
-
-bus = DBus.session_bus
-service = bus["org.mpris.MediaPlayer2.spotify"]
-object = service["/org/mpris/MediaPlayer2"]
-interface = object["org.mpris.MediaPlayer2.Player"]
-status = interface["PlaybackStatus"]
-song = interface["Metadata"]["xesam:title"]
-artist = interface["Metadata"]["xesam:artist"].first
-if status == "Playing"
-  puts "#{song} - #[bold]#{artist}#[nobold]"
-else
-  puts "Nothing playing :("
-end
-')
-
-if [[ $IS_MAC ]]; then
-  echo $MAC_NOW_PLAYING
-else
-  echo $LINUX_NOW_PLAYING
+  echo "Nothing playing ☹️ "
 fi
